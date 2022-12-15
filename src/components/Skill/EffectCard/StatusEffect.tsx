35b0899b20aa4b2a103bfab6628cd5368148bdc4
Edgar Nightingale <edgar.nightingale@geniussports.com>
2022-12-15 12:19:22 +0000

    Add effects to skill page

---
+++ src/components/Skill/EffectCard/StatusEffect.tsx
@@ -0,0 +1,60 @@
+import { Box, Typography } from "@mui/material";
+import { DamageType, Stat } from "../../../enums";
+import { IStatusEffect } from "../../../types";
+import { RESISTANCES_NAME_MAP, STATS_NAME_MAP } from "../../../utils";
+
+export const StatusEffect: React.FC<IStatusEffect> = ({
+	modifiers,
+	accuracy,
+	duration,
+}) => {
+	const { stats, resistances } = modifiers;
+
+	return (
+		<Box component="ul" sx={{ margin: 0 }}>
+			<Typography component="li">
+				<Box component="span" sx={{ fontWeight: "medium" }}>
+					Accuracy:{" "}
+				</Box>
+				{accuracy}%
+			</Typography>
+			<Typography component="li">
+				<Box component="span" sx={{ fontWeight: "medium" }}>
+					Duration:{" "}
+				</Box>
+				{duration} turns
+			</Typography>
+			<Typography component="li">
+				<Box component="span" sx={{ fontWeight: "medium" }}>
+					Stat Modifiers
+				</Box>
+				<Box component="ul">
+					{Object.keys(stats).map((stat) => (
+						<Typography component="li">
+							<Box component="span" sx={{ fontWeight: "medium" }}>
+								{STATS_NAME_MAP[stat as Stat]}:{" "}
+							</Box>
+							{stats[stat as Stat]}
+						</Typography>
+					))}
+				</Box>
+			</Typography>
+			<Typography component="li">
+				<Box component="span" sx={{ fontWeight: "medium" }}>
+					Resistance Modifiers
+				</Box>
+				<Box component="ul">
+					{Object.keys(resistances).map((resistance) => (
+						<Typography component="li">
+							<Box component="span" sx={{ fontWeight: "medium" }}>
+								{RESISTANCES_NAME_MAP[resistance as DamageType]}
+								:{" "}
+							</Box>
+							{resistances[resistance as DamageType]}%
+						</Typography>
+					))}
+				</Box>
+			</Typography>
+		</Box>
+	);
+};
