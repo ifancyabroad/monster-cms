40a4495eb3850de57d36e2f5449be4f5008f4db9
Edgar Nightingale <edgar.nightingale@geniussports.com>
2023-01-06 10:59:59 +0000

    Add dashboard cards

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,12 +1,73 @@
-import { Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
+import { DashboardCard } from "./DashboardCard";
+import monstersImage from "../../assets/images/monsters.jpg";
+import skillsImage from "../../assets/images/skills.png";
+import weaponsImage from "../../assets/images/weapons.webp";
+import armourImage from "../../assets/images/armour.jpg";
+import { useAppDispatch } from "../../app/hooks";
+import {
+	openMonsterModal,
+	openSkillModal,
+} from "../../features/modals/modalsSlice";
 
 export const Dashboard: React.FC = () => {
+	const dispatch = useAppDispatch();
+
+	const handleAddMonster = () => {
+		dispatch(openMonsterModal());
+	};
+
+	const handleAddSkill = () => {
+		dispatch(openSkillModal());
+	};
+
+	const handleAddWeapon = () => {};
+
+	const handleAddArmour = () => {};
+
 	return (
 		<div>
 			<Typography variant="h2" gutterBottom>
 				Dashboard
 			</Typography>
-			<Typography>Coming soon</Typography>
+			<Grid container spacing={2}>
+				<Grid item xs={12} sm={6} lg={3}>
+					<DashboardCard
+						title="Monsters"
+						description="A complete list of monsters"
+						image={monstersImage}
+						link="/monsters"
+						onAdd={handleAddMonster}
+					/>
+				</Grid>
+				<Grid item xs={12} sm={6} lg={3}>
+					<DashboardCard
+						title="Skills"
+						description="A complete list of skills"
+						image={skillsImage}
+						link="/skills"
+						onAdd={handleAddSkill}
+					/>
+				</Grid>
+				<Grid item xs={12} sm={6} lg={3}>
+					<DashboardCard
+						title="Weapons"
+						description="A complete list of weapons"
+						image={weaponsImage}
+						link="/weapons"
+						onAdd={handleAddWeapon}
+					/>
+				</Grid>
+				<Grid item xs={12} sm={6} lg={3}>
+					<DashboardCard
+						title="Armour"
+						description="A complete list of armour"
+						image={armourImage}
+						link="/armour"
+						onAdd={handleAddArmour}
+					/>
+				</Grid>
+			</Grid>
 		</div>
 	);
 };
